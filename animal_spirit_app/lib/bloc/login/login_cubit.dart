2694070eab25f796d13754d3902ed9dc87af7471
paulo46 <paulo@blubybs2.com>
2022-04-01 05:56:15 -0300

    Add some comments

--- animal_spirit_app/lib/bloc/login/login_cubit.dart
+++ animal_spirit_app/lib/bloc/login/login_cubit.dart
@@ -1,6 +1,12 @@
 import 'package:animal_spirit_app/bloc/login/login_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+/// Obs: For some reason the import of flutter bloc dint work properly
+/// the emit() function wont work and i dint figured out why
+/// kinda sad that i dint had time to use a new State Management
+/// Spend a lot of time to make it on Bloc :/ ;/
+/// But here is the code anyways
+
 /// The cubit class to manage the state of Login Page
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginState.initial());
--- animal_spirit_app/lib/data/fake_data_repository.dart
+++ animal_spirit_app/lib/data/fake_data_repository.dart
@@ -3,7 +3,11 @@ import 'dart:convert';
 import 'package:animal_spirit_app/model/animal.dart';
 import 'package:flutter/services.dart';
 
+/// Class to obatain our fake data
+///
 class FakeDataRepository {
+  /// Get all animals from our local json file
+  /// and convert in a list of the Animal Object
   Future<List<Animal>> getallAnimals() async {
     var json = await rootBundle.loadString('lib/data/animals_data.json');
     var parsed = jsonDecode(json.toString()).cast<Map<String, dynamic>>();
